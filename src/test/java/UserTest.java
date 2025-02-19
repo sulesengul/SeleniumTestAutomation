a72c29ae3e064b42f27bf5101932b923b0fd8828
sulesengul <sulesengul32@gmail.com>
2025-02-19 18:22:02 +0300

    Update UserTest.java

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 public class UserTest {
 
     @Test
-    public void loginSuccesful() throws InterruptedException {
+    public void loginSuccessful() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.trendyol.com/giris");
         driver.manage().window().fullscreen();
@@ -18,6 +18,19 @@ public class UserTest {
         String text=driver.findElements(By.cssSelector("[class='link-text']")).get(0).getText();
         System.out.println(text);
         Assert.assertEquals(text,"Hesabım");
+    }
+
+    @Test
+    public void loginUnsuccessful() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.trendyol.com/giris");
+        driver.manage().window().fullscreen();
+        driver.findElement(By.id("login-email")).sendKeys("atalayedamerve@gmail.com");
+        driver.findElement(By.name("login-password")).sendKeys("Testeda123");
+        driver.findElement(By.cssSelector("[class='q-primary q-fluid q-button-medium q-button submit']")).click();
+        Thread.sleep(5000);
+        System.out.println(driver.findElement(By.cssSelector("[class='message']")).getText());
+        //[class='message']
     }
 
 
